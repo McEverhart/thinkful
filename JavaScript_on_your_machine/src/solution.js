6c1d294c49fb8085563e5b8406c8fffc1b4958d9
McEverhart <McEverhartl@thinkful.com>
2024-01-23 16:24:58 -0700

    updt

---
+++ JavaScript_on_your_machine/src/solution.js
@@ -0,0 +1,13 @@
+function getUserName(inputName) {
+  if (inputName === "Maria") {
+    return "Hello, Maria!";
+  } else {
+    return "Hello, " + inputName + "!";
+  }
+}
+
+////////////////////////////////
+//// Ignore code below here.
+////////////////////////////////
+
+module.exports = getUserName;
